cce7951ec65d76d2e4d10db743b549b45bdf1a9c
sudobanana <bcosta2013@gmail.com>
2026-08-25 16:00:48 -0400

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+const CACHE = "taskmap-runtime-v5";
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["/"])));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith("taskmap-runtime-") && key !== CACHE).map(key => caches.delete(key)))),
+    self.clients.claim(),
+  ]));
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(async () => (await caches.match(event.request)) || (event.request.mode === "navigate" ? caches.match("/") : undefined)));
+});
